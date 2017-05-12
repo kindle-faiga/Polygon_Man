7c41ab17dd0e611b683167b4fa9e98888f0c9210
toyama-shuma <ts.gold.fire@gmail.com>
2017-05-12 17:58:28 +0900

    Add base system

---
+++ Assets/Script/ConveyorManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorManager : MonoBehaviour 
+{
+	[SerializeField]
+	private bool isRight = true;
+	[SerializeField] 
+	private float length = 0;
+	[SerializeField]
+	private float size = 0.5f;
+
+	void Start () 
+	{
+		GameObject wheelObject = Resources.Load("Prefab/Wheel") as GameObject;
+
+		Vector3 pos = transform.position;
+
+		if (!isRight)
+		{
+			size = -size;
+		}
+
+		for (int i = 0; i < length; ++i)
+		{
+			Vector3 p = new Vector3(pos.x + size*i, pos.y, pos.z);
+
+			GameObject wheel = Instantiate(wheelObject, p, transform.rotation) as GameObject;
+
+			wheel.transform.parent = transform;
+		}
+	}
+}
---
+++ Assets/Script/PlayerManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(PolygonCollider2D))]
+public class PlayerManager : MonoBehaviour 
+{
+	void Start () 
+	{
+		
+	}
+}
