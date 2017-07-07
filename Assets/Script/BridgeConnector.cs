abe3eea1dbd3eca82ecd726d0fb8de715f22a5b9
toyama-shuma <ts.gold.fire@gmail.com>
2017-07-07 16:45:00 +0900

    [add] 橋完成

--- Assets/Script/BridgeConnector.cs
+++ Assets/Script/BridgeConnector.cs
@@ -2,50 +2,117 @@
 
 namespace PolygonMan
 {
-    [RequireComponent(typeof(SpriteRenderer))]
+    enum BridgeState {Release, Select, Connect};
+
+[RequireComponent(typeof(SpriteRenderer))]
     public class BridgeConnector : MonoBehaviour
     {
-        private SpriteRenderer spriteRenderer;
-        private float depth = 10.0f;
-        private bool isSelected = false;
+        SpriteRenderer spriteRenderer;
+		BoxCollider2D col;
+        Vector2 defaultColSize;
+        Vector2 defaultColOffset;
+        float depth = 10.0f;
+        BridgeState state;
 
-        private void Start()
+        void Start()
         {
             spriteRenderer = GetComponent<SpriteRenderer>();
+            col = GetComponent<BoxCollider2D>();
+            defaultColSize = col.size;
+            defaultColOffset = col.offset;
+        }
+
+        RaycastHit2D IsSelected()
+        {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            return Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction, depth, 1 << LayerMask.NameToLayer("Bridge"));
+        }
+
+        void SetScale(Vector3 target)
+        {
+            Vector2 targetPos = new Vector2(target.x, target.y);
+			Vector2 position = new Vector2(transform.position.x, transform.position.y);
+			spriteRenderer.size = new Vector2(Vector2.Distance(targetPos, position) * 4, 1);
+        }
+
+        void SetAngle(Vector3 crum, Vector3 target)
+        {
+            Quaternion rotation = Quaternion.LookRotation(Vector3.forward, target - crum);
+			transform.localRotation = rotation;
+			transform.localEulerAngles = new Vector3(0, 0, transform.localEulerAngles.z + 90.0f);
+        }
+
+        void SetCollider(Vector2 size, Vector2 offset, string tagName)
+        {
+            col.offset = offset;
+            col.size = size;
+            transform.tag = tagName;
+        }
+
+        void Release()
+        {
+            state = BridgeState.Release;
+			spriteRenderer.size = Vector2.one;
+			transform.localEulerAngles = Vector3.one;
         }
 
         void Update()
 		{
 			if (Input.GetMouseButtonDown(0))
 			{
-				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-				RaycastHit2D hit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction, depth, 1 << LayerMask.NameToLayer("Bridge"));
+                RaycastHit2D hit = IsSelected();
 
                 if(hit && hit.collider.gameObject.Equals(gameObject))
                 {
-                    isSelected = true;
+                    switch(state)
+                    {
+                        case BridgeState.Release:
+                            state = BridgeState.Select;
+                            break;
+                        case BridgeState.Select:
+							break;
+                        case BridgeState.Connect:
+                            Release();
+                            SetCollider(col.size, col.offset, "Untagged");
+							break;
+                        default:
+                            break;
+                    }
                 }
 			}
 
 			if (Input.GetMouseButtonUp(0))
 			{
-                spriteRenderer.size = new Vector2(1,1);
-                transform.localEulerAngles = Vector3.one;
+                if (state.Equals(BridgeState.Select))
+				{
+                    RaycastHit2D hit = IsSelected();
+                    if (hit && !hit.collider.gameObject.Equals(gameObject))
+                    {
+                        state = BridgeState.Connect;
 
-                isSelected = false;
+                        Vector3 target = hit.collider.transform.position;
+                        SetScale(target);
+                        SetAngle(transform.position, target);
+
+                        Vector2 size = new Vector2(spriteRenderer.size.x + defaultColSize.x, defaultColSize.y);
+                        Vector2 offset = new Vector2(defaultColOffset.x+spriteRenderer.size.x/2, defaultColOffset.y);
+                        SetCollider(size, offset, "Ground");
+					}
+                    else
+                    {
+                        Release();
+                    }
+				}
 			}
 
 			if (Input.GetMouseButton(0))
 			{
-                if (isSelected)
+                if (state.Equals(BridgeState.Select))
                 {
-                    Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                    spriteRenderer.size = new Vector2(Vector2.Distance(new Vector2(mousePos.x, mousePos.y), new Vector2(transform.position.x, transform.position.y)) * 2, 1);
-
-                    Vector3 worldMousePos = Camera.main.WorldToScreenPoint(transform.position);
-                    Quaternion rotation = Quaternion.LookRotation(Vector3.forward, Input.mousePosition - worldMousePos);
-                    transform.localRotation = rotation;
-                    transform.localEulerAngles = new Vector3(0, 0, transform.localEulerAngles.z + 90.0f);
+                    Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    SetScale(mouse);
+                    Vector3 worldMouse = Camera.main.WorldToScreenPoint(transform.position);
+                    SetAngle(worldMouse, Input.mousePosition);
                 }
 			}
 		}
